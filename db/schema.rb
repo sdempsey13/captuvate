ff209ca0cd1bc7b6ab672a25a49b9565478bd08f
Steven Dempsey <steve@stevens-air.mynetworksettings.com>
2025-02-05 17:47:18 -0800

    Create pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_05_212032) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_06_010257) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "pages", force: :cascade do |t|
-    t.string "title"
+    t.bigint "user_id", null: false
+    t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_05_212032) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "pages", "users"
 end
