619256fa8880a8f2b8fe00168fdb043976d8c978
Steven Dempsey <steve@stevens-air.mynetworksettings.com>
2025-03-23 21:19:16 -0700

    Styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "home#index"
+  
   devise_for :users
 
   resources :domains
@@ -10,7 +12,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  root "home#index"
 end
