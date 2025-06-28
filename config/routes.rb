39389807ac7e20b2960af6f643d1477e3a380ffb
Steven Dempsey <steve@stevens-air.mynetworksettings.com>
2025-06-28 11:48:28 -0700

    Polished user sign up/organization flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
   
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
   resource :dashboard, only: [:show], controller: 'dashboard'
 
