5f5c210ca918ed47c13da70e8734a416949ff28f
Steven Dempsey <steve@stevens-air.mynetworksettings.com>
2025-06-29 10:55:54 -0700

    Add masked_api_key and add delete action to IC

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_28_185413) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_29_165711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -80,6 +80,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_28_185413) do
     t.text "encrypted_api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "masked_api_key"
     t.index ["integration_id"], name: "index_integration_credentials_on_integration_id"
     t.index ["organization_id"], name: "index_integration_credentials_on_organization_id"
   end
